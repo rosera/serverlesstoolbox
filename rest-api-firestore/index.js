0cec890f55fc94852626b745a82e52fc6fb62959
Roger Martinez <rgmartinez@google.com>
2020-04-02 14:59:29 -0700

    getBreeds function, add express webserver

--- rest-api-firestore/index.js
+++ rest-api-firestore/index.js
@@ -1,16 +1,35 @@
 const admin = require( 'firebase-admin' );
-
-let serviceAccount = require( './barkbark-key.json' );
+const express = require('express');
+const serviceAccount = require( './barkbark-key.json' );
+const app = express();
+const port = process.env.PORT || 8080;
 
 admin.initializeApp( {
     credential: admin.credential.cert( serviceAccount )
   } );
+const db = admin.firestore();
 
-let db = admin.firestore();
+app.listen(port, () => {
+    console.log('BarkBark Rest API listening on port', port);
+});
 
-async function getDogs() {
-    let response = await db.collection( "dogs" ).get()
-    return response.docs.map( doc => {
-        return { id: doc.id, ...doc.data() }
-    })
+async function getBreed(breed) {
+    let dogsRef = db.collection( "dogs" );
+    let response = await dogsRef.where( "name", "==", breed ).get();
+    return response
 }
+
+app.get('/:breed', async(req, res)=> {
+    const breed = req.params.breed;
+    const breedData = await getBreed(breed);
+    let retVal;
+    if (!breedData.empty) {
+        retVal = breedData.docs.map( doc =>{
+            return {...doc.data()}
+        })
+    } else {
+        res.status(404);
+        retVal = {  status: 'fail', data: {title: `${breed} not found`} };
+    }
+    res.json(retVal)
+})
