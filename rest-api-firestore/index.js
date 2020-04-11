724e338e1497fd32c2d56bbd00c88d6ddd5c4f43
Roger Martinez <rgmartinez@rgmartinez-macbookpro.roam.corp.google.com>
2020-04-10 17:39:35 -0700

    clean up and formatting

--- rest-api-firestore/index.js
+++ rest-api-firestore/index.js
@@ -5,23 +5,12 @@ const port = process.env.PORT || 8080;
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json())
+
 admin.initializeApp({
     credential: admin.credential.applicationDefault()
 });
-/* 
-The initializeApp function above will use the current project's Application Default
-Credentials.  You can specify a different service account by providing the service
-account key in a json file and initializing with that instead:
- 
-const serviceAccount = require('./barkbark-key.json');
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-});
-
-*/
 const db = admin.firestore();
-app.use('/add', express.static('static'));
-app.use(express.urlencoded());
+
 app.listen(port, () => {
     console.log('BarkBark Rest API listening on port', port);
 });
@@ -45,5 +34,5 @@ app.post('/', async (req, res) => {
         type: req.body.type
     }
     await db.collection('dogs').doc().set(data);
-    res.json({status: 'success', data: {dog: data}});
+    res.json({ status: 'success', data: { dog: data } });
 })
