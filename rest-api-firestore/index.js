3cc53d11942f02b8f8fa256903967ac2a03e7d6b
Roger Martinez <rgmartinez@google.com>
2020-04-17 11:00:12 -0700

    use tamplate literals

--- rest-api-firestore/index.js
+++ rest-api-firestore/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.json())
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log('BarkBark Rest API listening on port', port);
+    console.log(`BarkBark Rest API listening on port ${port}`);
 });
 
 admin.initializeApp({
