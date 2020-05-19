2f7927c09bb8c3c24ea16577fe7f50a5f5e6e7a3
Rich Rose <askrichardrose@gmail.com>
2020-05-19 13:54:18 +0100

    Add missing code to repo - Cloud PubSub

---
+++ async-cloud-pubsub/order-service/index.js
@@ -0,0 +1,36 @@
+const Firestore = require('@google-cloud/firestore');
+const db = new Firestore();
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+})
+
+
+app.post('/', async (req, res) => {
+  const data = req.body.message.data;
+  const order = decodeBase64Json(data);
+
+  console.log(`ORDER: ${order}`);
+
+  await saveOrder(order);
+  res.status(204).send();
+}
+
+
+function decodeBase64Json(data) {
+  return JSON.parse(
+    Buffer.from(data, 'base64').toString();
+  );
+}
+
+async function saveOrder(order) {
+  const id = order.id.toString();
+  console.log ('Writing to Db');
+//  await db.collection('orders').doc(id).set(order);
+}
+
+
