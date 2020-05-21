d68641ac14c5824d5975037a1ee300ddc42414ff
Rich Rose <askrichardrose@gmail.com>
2020-05-21 08:12:43 +0100

    Missing module - body-parser

--- async-cloud-pubsub/order-service/index.js
+++ async-cloud-pubsub/order-service/index.js
@@ -1,9 +1,11 @@
 const Firestore = require('@google-cloud/firestore');
 const db = new Firestore();
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-
 const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
