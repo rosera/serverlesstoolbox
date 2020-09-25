22fd2fb36c4fb18787949811f70106da80c49da4
Grant Timmerman <timmerman+devrel@google.com>
2020-09-25 14:32:09 -0500

    refactor: remove dockerfile from functions sample
    Signed-off-by: Grant Timmerman <timmerman+devrel@google.com>

--- critter-watch/critter-api/deploy.sh
+++ critter-watch/critter-api/deploy.sh
@@ -1,6 +1,7 @@
 GOOGLE_PROJECT_ID=critterwatcher
 
-gcloud builds submit --tag gcr.io/$GOOGLE_PROJECT_ID/critterwatch-rest-api \
+gcloud builds submit \
+  --tag gcr.io/$GOOGLE_PROJECT_ID/critterwatch-rest-api \
   --project=$GOOGLE_PROJECT_ID
 
 gcloud run deploy critterwatch-rest-api \
